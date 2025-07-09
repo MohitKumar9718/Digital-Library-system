12db5d7cdb0953de6d753c658a89fcebbd4963a4
MohitKumar9718 <218480125+MohitKumar9718@users.noreply.github.com>
2025-07-10 00:22:43 +0530

    Final setup with runtime.txt and createsu command

--- library/management/commands/createsu.py
+++ library/management/commands/createsu.py
@@ -6,9 +6,9 @@ class Command(BaseCommand):
     help = 'Creates a default superuser.'
 
     def handle(self, *args, **kwargs):
-        username = os.getenv('DJANGO_SUPERUSER_USERNAME', 'admin')
-        email = os.getenv('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
-        password = os.getenv('DJANGO_SUPERUSER_PASSWORD', 'admin123')
+        username = os.getenv('DJANGO_SUPERUSER_USERNAME', 'Mohit')
+        email = os.getenv('DJANGO_SUPERUSER_EMAIL', 'mohitmacpherson@gmail.com')
+        password = os.getenv('DJANGO_SUPERUSER_PASSWORD', 'Mohit9718358279@')
 
         if not User.objects.filter(username=username).exists():
             User.objects.create_superuser(username=username, email=email, password=password)
